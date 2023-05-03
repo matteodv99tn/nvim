757c0227426c33383e020467dacf7ee977cf2057
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-05-03 09:38:23 +0200

    Added doge For code documentation

---
+++ after/plugin/doge.lua
@@ -0,0 +1,9 @@
+vim.g.doge_doc_standard_python = 'numpy' 
+vim.g.doge_doc_standard_cpp = 'doxygen_javadoc'
+vim.g.doge_comment_interactive = true
+
+
+vim.g.doge_python_settings = {
+    single_quotes= false,
+    omit_redundant_param_types= false ,
+}
--- lua/matteodv99/packer.lua
+++ lua/matteodv99/packer.lua
@@ -73,6 +73,8 @@ return require('packer').startup(function(use)
     -- Copilot
     use("github/copilot.vim")
 
+
+    use{'kkoomen/vim-doge', run = ':call doge#install()'}
     use({ "pavanbhat1999/figlet.nvim", requires = "numToStr/Comment.nvim"})
 end)
 
